ff37ed0eccbf4a4cde7f2fe0e5641db00fde20b6
raj sharma <rajsharma63680@gmail.com>
2025-02-12 19:17:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Form Validation
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const name = document.querySelector('input[type="text"]').value;
+    const email = document.querySelector('input[type="email"]').value;
+    const phone = document.querySelector('input[type="tel"]').value;
+    const country = document.querySelector('select').value;
+  
+    if (name && email && phone && country) {
+      alert('Form submitted successfully!');
+    } else {
+      alert('Please fill all fields.');
+    }
+  });
